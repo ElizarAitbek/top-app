accd52cf2292fa1b21f2540760d67670e54f6e2c
Elizar Aitbek uulu <elizar.aitbek@gmail.com>
2023-04-20 22:07:21 +0600

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
+   return (
+      <>
+         <Head>
+            <title>My top app</title>
+            <link rel="icon" href="/public/favicon.ico" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+         </Head>
+         <Component {...pageProps} />
+      </>
+   );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@components/index";
+
+export default function Home(): JSX.Element {
+   return (
+      <>
+         <Button appearance="ghost">Click me</Button>
+      </>
+   );
+}
