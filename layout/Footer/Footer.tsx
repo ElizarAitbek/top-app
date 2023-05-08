9e9c5ba71913fe0ae358c638218e1e80ac5dc779
Elizar Aitbek <elizar.aitbek@gmail.com>
2023-05-08 18:15:23 +0600

    add a layout and footer styles

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
+import { format } from "date-fns";
+import cn from "classnames";
 
-export const Footer = ({ ...props }: FooterProps) => {
-  return <div {...props}>Footer component</div>;
+export const Footer = ({ className, ...props }: FooterProps) => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <p>OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены</p>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
